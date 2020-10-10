80151e30c4903989bf9b38f385f5c9406620364c
cuiwenzhe <xhrmanrs@163.com>
2020-10-10 15:05:02 +0800

    更改了一些数据

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-我是zhuye1
+    
+    <header>
+      <div class="header_left"></div>
+
+    </header>
   </div>
 </template>
 
@@ -16,5 +20,16 @@
 </script>
 
 <style lang="less" scoped>
+header{
+  width: 100%;
+  height: 1.2rem;
+  background:#F2F2F2;
+  display: flex;
+  justify-content: space-between;
+  .header_left{
+    
+  }
+
+}
 
 </style>
