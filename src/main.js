d6d5202e1d9382328d7f2da4e28e3285630b53cb
cuiwenzhe <xhrmanrs@163.com>
2020-10-10 14:26:02 +0800

    创建了tabbar

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/icon/iconfont.css' //阿里图标库
+//引入axios
+import axios from 'axios'
+//引入less
+import less from 'less'
+//引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//引入写手机端的样式
+
+import './assets/css/style.css'
+import './assets/css/normalize.css'
+import './assets/js/rem.js'
+
+Vue.use(less)
+Vue.use(Vant)
+
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
